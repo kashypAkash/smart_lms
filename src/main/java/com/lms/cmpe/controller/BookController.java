079efdb210c2fde672f17397baa1ea04eb98867a
akash <akash.kashyap@sjsu.edu>
2016-11-30 20:11:32 -0800

    bookkeywords bookid value issue fixed

--- src/main/java/com/lms/cmpe/controller/BookController.java
+++ src/main/java/com/lms/cmpe/controller/BookController.java
@@ -4,6 +4,7 @@ import com.lms.cmpe.model.Book;
 import com.lms.cmpe.model.BookKeywords;
 import com.lms.cmpe.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -34,7 +35,7 @@ public class BookController {
     }
 
     @RequestMapping(value = "/book/add", method = RequestMethod.POST)
-    public String addBook(@ModelAttribute Book book, Model model, @RequestParam(value="action") String action,HttpSession session){
+    public String addBook(@ModelAttribute Book book, Model model, @RequestParam(value="action") String action, HttpSession session){
         model.addAttribute("user",session.getAttribute("user"));
         if(action.equals("addkeyword")){
             book.getBookKeywordsList().add(new BookKeywords());
@@ -44,6 +45,7 @@ public class BookController {
 
         if(action.equals("add")){
             System.out.println(book.toString());
+            book.addBookKeywords();
             bookService.addBook(book);
             return "test";
         }
@@ -63,12 +65,21 @@ public class BookController {
         return "book";
     }
 
+    @GetMapping("/book/update/{id}")
+    public String bookUpdateForm(@PathVariable("id") int id, Model model, HttpSession session){
+        model.addAttribute("book", bookService.getBookById(id));
+        model.addAttribute("user", session.getAttribute("user"));
+        return "updatebook";
+    }
+
     @RequestMapping(value = "/book/update",method = RequestMethod.POST)
     public String updateBook(@ModelAttribute Book book, @PathVariable("id") int id,
-                             @RequestParam(value="action", required=true) String action){
-
-       book.setBookId(id);
-
+                             @RequestParam(value="action", required=true) String action, HttpSession session,
+                             Model model){
+        model.addAttribute("user",session.getAttribute("user"));
+        System.out.println(book.toString());
+        book.setBookId(id);
+        System.out.println(book.toString());
         if(action.equals("update")){
             bookService.updateBook(book);
         }
