254737720fb6ea4c5c9b41cd2666b9818a3f6349
akash <akash.kashyap@sjsu.edu>
2016-12-06 20:02:00 -0800

    role checkbox updated

--- src/main/java/com/lms/cmpe/controller/LoginController.java
+++ src/main/java/com/lms/cmpe/controller/LoginController.java
@@ -1,17 +1,16 @@
 package com.lms.cmpe.controller;
 
-import com.lms.cmpe.model.Book;
 import com.lms.cmpe.model.User;
 import com.lms.cmpe.service.MailService;
 import com.lms.cmpe.service.UserService;
 import com.lms.cmpe.service.VerificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpSession;
 
@@ -65,8 +64,12 @@ public class LoginController {
     }
 
     @PostMapping("/signup")
-    public String createUser(@ModelAttribute User user){
+    public String createUser(@ModelAttribute User user,@RequestParam(value="userRole", required=false) String role){
         user.setVerificationCode(Integer.toString(verificationService.verficationCode()));
+        if(role != null){
+            user.setUserRole(role);
+        }
+
         userService.saveUser(user);
         mailService.sendMail(user);
         return "activate";
