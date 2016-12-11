6fb34af2dd6187fff34ad63a27abdedbb39b6ae5
akash <akash.kashyap@sjsu.edu>
2016-12-10 20:51:09 -0800

    flash feedback added for add, update, deletion of book

--- src/main/java/com/lms/cmpe/controller/BookController.java
+++ src/main/java/com/lms/cmpe/controller/BookController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
@@ -53,7 +54,7 @@ public class BookController {
     }
 
     @GetMapping("/book")
-    public String bookForm(Model model,HttpSession session){
+    public String bookForm(Model model, HttpSession session){
 
         if(session.getAttribute("user")==null){
             return "redirect:/";
@@ -62,11 +63,13 @@ public class BookController {
         Book book = new Book();
         model.addAttribute("user",session.getAttribute("user"));
         model.addAttribute("book",book);
+
         return "book";
     }
 
     @RequestMapping(value = "/book/add", method = RequestMethod.POST)
-    public String addBook(@ModelAttribute Book book, Model model, @RequestParam(value="action") String action, HttpSession session){
+    public String addBook(@ModelAttribute Book book, Model model, @RequestParam(value="action") String action,
+                          HttpSession session, RedirectAttributes redirectAttributes){
         if(session.getAttribute("user")==null){
             return "redirect:/";
         }
@@ -84,7 +87,8 @@ public class BookController {
             int total = book.getNoOfAvailableCopies();
             book.setNoOfAvailableCopies(total);
             bookService.addBook(book);
-            return "redirect:/book";
+            redirectAttributes.addFlashAttribute("message", "Successfully Added! Add more books..");
+            return"redirect:/book";
         }
         return "test";
     }
@@ -116,7 +120,7 @@ public class BookController {
     @RequestMapping(value = "/book/update/{id}",method = RequestMethod.POST)
     public String updateBook(@ModelAttribute Book book, @PathVariable("id") int id,
                              @RequestParam(value="action", required=true) String action, HttpSession session,
-                             Model model){
+                             Model model, RedirectAttributes redirectAttributes){
         if(session.getAttribute("user")==null){
             return "redirect:/";
         }
@@ -134,19 +138,21 @@ public class BookController {
         if(action.equals("update")){
             book.addBookKeywords();
             bookService.updateBook(book);
+            redirectAttributes.addFlashAttribute("message","Book info is updated");
         }
 
         return String.format("redirect:/book/update/%d",book.getBookId());
     }
 
     @GetMapping("/book/delete/{id}")
-    public String deleteBookByid(@PathVariable("id") int id, HttpSession session){
+    public String deleteBookByid(@PathVariable("id") int id, HttpSession session, RedirectAttributes redirectAttributes){
         if(session.getAttribute("user")==null){
             return "redirect:/";
         }
 
         Book book= bookService.getBookById(id);
         bookService.deleteBook(book);
+        redirectAttributes.addFlashAttribute("message", "Book has been deleted");
         return "redirect:/books";
     }
 
